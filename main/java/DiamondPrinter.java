d69d61aeff1da98943f198c15798c9dd0b1aa2f8
Saena <toastyhatesthesea@gmail.com>
2018-08-21 14:33:58 -0700

    Creating Class, added HowLargeIsDiamond Function

---
+++ main/java/DiamondPrinter.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+class DiamondPrinter {
+
+    List<String> printToList(char a) {
+
+        return null;
+    }
+
+
+    /**
+     * Determines the size of the diamond based ont he character input
+     * @param a char
+     * @return int
+     */
+    public int howLargeIsDiamond(char a)
+    {
+        a = Character.toUpperCase(a);
+
+        return a - 'A';
+    }
+
+}
+
+
