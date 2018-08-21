132893fc8baeb7bf1ec9a7998dbafc73b7751282
Saena <toastyhatesthesea@gmail.com>
2018-08-21 15:24:32 -0700

    Added oneLine Function and GetSpaces Function

--- main/java/DiamondPrinter.java
+++ main/java/DiamondPrinter.java
@@ -1,10 +1,46 @@
+import java.util.ArrayList;
 import java.util.List;
 
 class DiamondPrinter {
 
     List<String> printToList(char a) {
 
+
+
         return null;
+    }
+
+    public List<String> oneLine(char a, int padding)
+    {
+        String aString = "";
+
+        if (padding > 0)
+        {
+            String totalSpaces = getSpaces(padding);
+            aString = aString.concat(totalSpaces);
+            aString = aString.concat(Character.toString(a));
+            aString = aString.concat(totalSpaces);
+        }
+        else
+        {
+            aString = aString.concat(Character.toString(a));
+        }
+
+        List answer = new ArrayList<String>();
+        answer.add(aString);
+
+        return answer;
+    }
+
+    public String getSpaces(int num)
+    {
+        String answer = "";
+
+        for (int i = 0; i < num; i++)
+        {
+            answer = answer.concat(" ");
+        }
+        return answer;
     }
 
 
@@ -17,9 +53,21 @@ class DiamondPrinter {
     {
         a = Character.toUpperCase(a);
 
-        return a - 'A';
+        return a - 'A' + 1;
     }
+
+
 
 }
 
+class DiamondTesters
+{
+    public static void main(String[] argsdgsg)
+    {
+        DiamondPrinter dia = new DiamondPrinter();
 
+        int size = dia.howLargeIsDiamond('Z');
+    }
+
+
+}
