a668b778af7beef16048044157300de7f82ff8a1
Saena <toastyhatesthesea@gmail.com>
2018-08-21 17:42:36 -0700

    Added Loops to printToList

--- main/java/DiamondPrinter.java
+++ main/java/DiamondPrinter.java
@@ -5,11 +5,34 @@ class DiamondPrinter {
 
     List<String> printToList(char a) {
 
+        int totalSize = howLargeIsDiamond(a);
+        List answer = new ArrayList();
+        Character starterLetter = 'A';
 
 
-        return null;
+        for (int i = 0; i <= totalSize; i++)
+        {
+            answer.add(oneLine(starterLetter, totalSize - i));
+            starterLetter = (char) (starterLetter + 1);
+        }
+
+        starterLetter = (char) (starterLetter - 1);
+
+        for (int i = 1; i <= totalSize; i++)
+        {
+            answer.add(oneLine(starterLetter, i));
+            starterLetter = (char) (starterLetter - 1);
+        }
+
+        return answer;
     }
 
+    /**
+     * Prints one line for the Diamond shape
+     * @param a char
+     * @param padding int
+     * @return List
+     */
     public List<String> oneLine(char a, int padding)
     {
         String aString = "";
@@ -19,6 +42,15 @@ class DiamondPrinter {
             String totalSpaces = getSpaces(padding);
             aString = aString.concat(totalSpaces);
             aString = aString.concat(Character.toString(a));
+            if (a != 'a')
+            {
+                int totalSpacesBetweenChars = a - 'A';
+                String inBetweenSpaces = getSpaces(totalSpacesBetweenChars);
+
+                aString = aString.concat(inBetweenSpaces);
+                aString = aString.concat(Character.toString(a));
+            }
+
             aString = aString.concat(totalSpaces);
         }
         else
@@ -53,7 +85,7 @@ class DiamondPrinter {
     {
         a = Character.toUpperCase(a);
 
-        return a - 'A' + 1;
+        return a - 'A';
     }
 
 
@@ -67,6 +99,12 @@ class DiamondTesters
         DiamondPrinter dia = new DiamondPrinter();
 
         int size = dia.howLargeIsDiamond('Z');
+
+        List meow = dia.oneLine('A', 3);
+
+        //System.out.println("A" + dia.getSpaces(3) + "A");
+
+        dia.printToList('B');
     }
 
 
